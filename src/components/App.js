90c7bac4be18d5d5fb3a6017e30e303ad3ba0dce
jerostax <jeremy.geneste@gmail.com>
2019-05-07 11:03:34 +0200

    Added images State to App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = { images: [] };
+  
   async onSearchSubmit(term) {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
@@ -10,13 +12,15 @@ class App extends React.Component {
         Authorization: 'Client-ID 3678a4c4dfd11bd29d20c2d0aa2db5210bd140c2a8e7f7a6149c427266750539'
       }
     });
-    console.log(response.data.results);
+
+    this.setState({ images: response.data.results });
   }
 
   render() { 
     return (
       <div className="ui container" style={{marginTop: '50px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Résultat: {this.state.images.length} images
       </div>
     );
   }
